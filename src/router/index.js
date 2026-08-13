c73086441b70b3ebe00bea1bbb9f5eb59930b59a
윤성 <gimyunseong173@gmail.com>
2026-08-13 16:34:16 +0900

    Use Pages-safe hash routing

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import WeatherHomeView from '../views/WeatherHomeView.vue'
 
 const routes = [
@@ -25,7 +25,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes,
 })
 
